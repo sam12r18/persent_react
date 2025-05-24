7f3954ca4b9b101d33c06f40819370a82e47e5bf
Tocka69 <am9268510@gmail.com>
2025-05-24 15:36:58 +0330

    connect routes to backend

--- src/Components/Pages/InviteBox.jsx
+++ src/Components/Pages/InviteBox.jsx
@@ -1,22 +1,55 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Container, Image } from "react-bootstrap";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useServer } from "../../AppContext.jsx";
 
 export default function InviteBox() {
-  const { data } = useParams();
+  // const { data } = useParams();
   const role = "admin";
+  const [invites, setInvites] = useState([]);
+  const [company, setCompany] = useState([]);
+  const { serverAddress } = useServer();
 
   useEffect(() => {
-    console.log(data, "its data");
+    // console.log(data, "its data");
+    const InvitesData = async () => {
+      try {
+        const Token = localStorage.getItem("authToken");
+        const suggest = await axios.get(`${serverAddress}suggest`, {
+          headers: {
+            Authorization: `Bearer ${Token}`,
+          },
+        });
+        const company = await axios.get(`${serverAddress}company`, {
+          headers: {
+            Authorization: `Bearer ${Token}`,
+          },
+        });
+
+        console.log("Thi is Data suggest:", suggest);
+        console.log("Thi is Data company:", company);
+
+        setInvites(suggest?.data?.data);
+        setCompany(company?.data?.company);
+        console.log(company?.data, "its response");
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    InvitesData();
     // اگر لازم شد از data برای گرفتن دعوت از سرور استفاده کن
     // مثال:
     // axios.get(`/api/invite/${data}`).then(...);
-  }, [data]);
+  }, []);
 
-  const acceptRequest = () => {
-    console.log("دعوت پذیرفته شد");
-    // اینجا می‌تونی درخواست تایید رو بفرستی به سرور با axios
+  const acceptRequest = async (id) => {
+    try {
+      const response = await axios.post(`${serverAddress}`, {});
+      console.log(response?.data, "its response");
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   const rejectRequest = () => {
@@ -43,39 +76,63 @@ export default function InviteBox() {
 
         <div>
           <span className="fs-4 fw-bold ms-3">دعوت شده اید به عنوان</span>
+          {Array.isArray(invites) &&
+            invites.length > 0 &&
+            invites.map((item) => (
+              <div
+                className="d-flex bg-orange justify-content-between rounded-4 p-3 mt-3"
+                key={item?.id}
+              >
+                <span className="text-white fs-5 fw-bold">{item?.name}</span>
+                <div className="gap-3 d-flex">
+                  <button
+                    onClick={acceptRequest}
+                    className="btn rounded-5 text-white btn-green"
+                  >
+                    تایید
+                  </button>
+                  <button
+                    onClick={rejectRequest}
+                    className="btn rounded-5 text-white btn-red"
+                  >
+                    لغو
+                  </button>
+                </div>
+              </div>
+            ))}
 
-          <div className="d-flex bg-orange justify-content-between rounded-4 p-3 mt-3">
-            <span className="text-white fs-5 fw-bold">کارمند مایکروسافت</span>
-            <div className="gap-3 d-flex">
-              <button
-                onClick={acceptRequest}
-                className="btn rounded-5 text-white btn-green"
-              >
-                تایید
-              </button>
-              <button
-                onClick={rejectRequest}
-                className="btn rounded-5 text-white btn-red"
-              >
-                لغو
-              </button>
-            </div>
-          </div>
           <div className="d-flex bg-gray justify-content-between rounded-4 p-3 mt-3 mb-3">
             <span className="text-gray fs-5 fw-bold">
               هیچ دعوتی برای شما ثبت نشده است
             </span>
           </div>
-          <span className="fs-4 fw-bold ms-3 mt-3">شرکت های شما </span>
-          <div className="d-flex bg-orange justify-content-between rounded-4 p-3 mt-2">
-            <span className="text-white fs-5 fw-bold">شرکت رایاسان </span>
-            {role === "admin" && (
-              <Link to="/admin-dashboard" className="btn fs-6 px-3 text-decoration-none text-white btn-green rounded-5">ورود</Link>
-            )}
-            {role === "employee" && (
-              <Link to="/registration-of-attendance" className="btn fs-6 px-3 text-decoration-none text-white btn-green rounded-5">ورود </Link>
-            )}
-          </div>
+          {company.length > 0 && (
+            <>
+              <span className="fs-4 fw-bold ms-3 mt-3">شرکت های شما</span>
+              <div className="d-flex bg-orange justify-content-between rounded-4 p-3 mt-2">
+                <span className="text-white fs-5 fw-bold">شرکت رایاسان</span>
+
+                {role === "admin" && (
+                  <Link
+                    to="/admin-dashboard"
+                    className="btn fs-6 px-3 text-decoration-none text-white btn-green rounded-5"
+                  >
+                    ورود
+                  </Link>
+                )}
+
+                {role === "employee" && (
+                  <Link
+                    to="/registration-of-attendance"
+                    className="btn fs-6 px-3 text-decoration-none text-white btn-green rounded-5"
+                  >
+                    ورود
+                  </Link>
+                )}
+              </div>
+            </>
+          )}
+
           <div className="my-4 d-flex flex-column">
             <span className="fs-4 fw-bold mt-3">
               کسب و کار خود را ایجاد کنید
