43e445c97ae602bd1ea18254bbe1aa4d21189022
Tocka69 <am9268510@gmail.com>
2025-09-09 15:44:56 +0330

    add Undefined component and connect employee list to server

---
+++ src/Components/public/Undefined.jsx
@@ -0,0 +1,9 @@
+export default function Undefined({title}) {
+    return(
+        <div className="d-flex bg-gray justify-content-between rounded-4 p-3 mt-3 mb-3">
+            <span className="text-gray fs-5 fw-bold">
+              {title}
+            </span>
+        </div>
+    )
+}
