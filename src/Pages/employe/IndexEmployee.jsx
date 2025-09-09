70f445fb43e29d519cbc9511a050488e61ac86d1
Tocka69 <am9268510@gmail.com>
2025-09-09 12:34:59 +0330

    add my task page for employee

---
+++ src/Pages/employe/IndexEmployee.jsx
@@ -0,0 +1,7 @@
+import {Outlet} from "react-router-dom";
+
+export default function IndexEmployee() {
+    return (
+        <Outlet/>
+    )
+}
---
+++ src/Pages/employe/MyTasks.jsx
@@ -0,0 +1,90 @@
+import { Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import {apiGet, apiPost} from "../../services/AxiosClient.jsx";
+import PageTitle from "../../Components/public/PageTitle.jsx";
+import useAlert from "../../hook/Alert.jsx";
+import TasksCard from "../../Components/public/TasksCard.jsx";
+
+export default function MyTasks() {
+  const [tasks, setTasks] = useState();
+  const alert = useAlert();
+  const fetchData = async () => {
+    const response = await apiGet(``, {});
+    console.log("this is response for tasks", response);
+    setTasks(response?.data);
+  };
+    // useEffect(() => {
+    //     fetchData();
+    // }, []);
+    const endTask = async (id) => {
+        alert({
+            title: "هشدار",
+            text: `آیا مطمئن هستید میخواهید این وظیفه را به اتمام برسانید؟`,
+            icon: "info",
+            showCancelButton: true,
+            confirmButtonText: "بله، تأیید می‌کنم",
+            cancelButtonText: "خیر، لغو کن",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                apiPost("", { task_id: id })
+                    .then(() => {
+                        alert({
+                            title: "موفقیت",
+                            text: `وظیفه شما به اتمام رسید`,
+                            icon: "success",
+                        });
+                    })
+                    .catch((err) => {
+                        alert({
+                            icon: "error",
+                            title: "خطا",
+                            text:
+                                err?.response?.data?.message ||
+                                "خطای ناشناخته‌ای رخ داد",
+                        });
+                    });
+            } else if (result.dismiss === "cancel") {
+                alert({
+                    title: "لغو شد",
+                    text: "هیچ تغییری در وضعیت وظیفه ایجاد نشد",
+                    icon: "error",
+                });
+            }
+        });
+    }
+    const needMoreTime = async (id)=>{
+        const response = await apiPost(``, {
+            task_Id:id
+        });
+    }
+
+    const myTasks = [
+        {
+            id:1,
+            project_name:"پروژه 1",
+            name:"کامل کردن صفحه لاگین",
+            deadline: "1404/06/13",
+            description:"لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای"
+        },
+        {
+            id:1,
+            project_name:"پروژه 1",
+            name:"کامل کردن صفحه لاگین",
+            deadline: "1404/06/13",
+            description:"لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای"
+        },
+        {
+            id:1,
+            project_name:"پروژه 1",
+            name:"کامل کردن صفحه لاگین",
+            deadline: "1404/06/13",
+            description:"لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای"
+        }
+    ]
+  return (
+    <Container className="container-sm mt-3" style={{ direction: "rtl" }}>
+        <PageTitle title={"لیست وظیفه های من"}/>
+        <TasksCard data={myTasks} endTask={endTask} needMoreTime={needMoreTime}/>
+    </Container>
+  );
+}
