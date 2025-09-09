fcae228f4b8cb8cd834e67a6e54379d984924588
Tocka69 <am9268510@gmail.com>
2025-09-09 12:29:45 +0330

    add invite box for manage company invite and companies

--- src/Pages/InviteBox.jsx
+++ src/Pages/InviteBox.jsx
@@ -1,154 +1,128 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { Container, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { useServer } from "../AppContext.jsx";
-
+import {apiGet, apiPost} from "../services/AxiosClient.jsx";
+import useAlert from "../hook/Alert.jsx";
+import {useAuth} from "../Context/AuthContext.jsx";
 export default function InviteBox() {
-  // const { data } = useParams();
-  const role = "admin";
   const [invites, setInvites] = useState([]);
-  const [company, setCompany] = useState([]);
-  const { serverAddress } = useServer();
+  const [companies, setCompanies] = useState([]);
+  const alert = useAlert();
+    const {setCompanyData} = useAuth()
+  const fetchData = async () => {
+        try {
+            const response = await apiGet(`suggest`, {});
+            const companies = await apiGet(`company`, {});
+            console.log("Thi is Data suggest:", response);
+            console.log("Thi is Data companies:", companies);
+            setInvites(response?.data)
+            setCompanies(companies?.data)
+        } catch (err) {
+            console.error("API error: ", err);
 
+        }
+  }
   useEffect(() => {
-    // console.log(data, "its data");
-    const InvitesData = async () => {
-      try {
-        const Token = localStorage.getItem("authToken");
-        const suggest = await axios.get(`${serverAddress}suggest`, {
-          headers: {
-            Authorization: `Bearer ${Token}`,
-          },
-        });
-        const company = await axios.get(`${serverAddress}company`, {
-          headers: {
-            Authorization: `Bearer ${Token}`,
-          },
-        });
-
-        console.log("Thi is Data suggest:", suggest);
-        console.log("Thi is Data company:", company);
-
-        setInvites(suggest?.data?.data);
-        setCompany(company?.data?.company);
-        console.log(company?.data, "its response");
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    InvitesData();
-    // اگر لازم شد از data برای گرفتن دعوت از سرور استفاده کن
-    // مثال:
-    // axios.get(`/api/invite/${data}`).then(...);
+      fetchData();
   }, []);
 
-  const acceptRequest = async (id) => {
-    try {
-      const response = await axios.post(`${serverAddress}`, {});
-      console.log(response?.data, "its response");
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const rejectRequest = () => {
-    console.log("دعوت رد شد");
-    // اینجا هم برای رد دعوت
+  const handleSuggested = async (id , status , name) => {
+      alert({
+          title: "هشدار",
+          text: `آیا مطمئن هستید میخواهید درخواست عضویت خود در شرکت ${name} را ${status === "accept" ? "تایید" : "رد"} کنید ؟!`,
+          icon: "info",
+          showCancelButton: true,
+          confirmButtonText: "بله، تأیید می‌کنم",
+          cancelButtonText: "خیر، لغو کن",
+      }).then((result) => {
+          if (result.isConfirmed) {
+              try {
+                  const response = apiPost("suggest/action", { suggest_id: id , action:status })
+                  console.log("this is response" , response)
+                  alert({
+                      title: "موفقیت",
+                      text: `درخواست با ${status === "accept" ? "تایید" : "رد"} موفقیت شد`,
+                      icon: "success",
+                  });
+                  fetchData();
+              }catch(err){
+                  alert({
+                      icon: "error",
+                      title: "خطا",
+                      text:
+                          err?.response?.data?.message ||
+                          "خطای ناشناخته‌ای رخ داد",
+                  });
+              }
+          }
+      });
   };
 
   return (
     <Container className="container-sm align-items-center justify-content-center mt-3">
-      <div className="row">
         <div className="d-flex mb-4 mt-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            fill="currentColor"
-            className="bi bi-emoji-smile-upside-down-fill me-3"
-            viewBox="0 0 16 16"
-          >
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-emoji-smile-upside-down-fill me-3" viewBox="0 0 16 16">
             <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M7 9.5C7 8.672 6.552 8 6 8s-1 .672-1 1.5.448 1.5 1 1.5 1-.672 1-1.5M4.285 6.433a.5.5 0 0 0 .683-.183A3.5 3.5 0 0 1 8 4.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.5 4.5 0 0 0 8 3.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683M10 8c-.552 0-1 .672-1 1.5s.448 1.5 1 1.5 1-.672 1-1.5S10.552 8 10 8" />
           </svg>
           <span className="fs-5">کاربر میهمان</span>
         </div>
-
         <div>
           <span className="fs-4 fw-bold ms-3">دعوت شده اید به عنوان</span>
-          {Array.isArray(invites) &&
-            invites.length > 0 &&
-            invites.map((item) => (
-              <div
-                className="d-flex bg-orange justify-content-between rounded-4 p-3 mt-3"
-                key={item?.id}
-              >
-                <span className="text-white fs-5 fw-bold">{item?.name}</span>
-                <div className="gap-3 d-flex">
-                  <button
-                    onClick={acceptRequest}
-                    className="btn rounded-5 text-white btn-green"
-                  >
-                    تایید
-                  </button>
-                  <button
-                    onClick={rejectRequest}
-                    className="btn rounded-5 text-white btn-red"
-                  >
-                    لغو
-                  </button>
+            {invites.length > 0 ? (
+                invites.map((invite) => (
+                    <div className="d-flex bg-orange justify-content-between rounded-4 p-3 mt-3" key={invite?.id}>
+                        <span className="text-white fs-5 fw-bold">{invite?.name}</span>
+                        <div className="d-flex gap-3">
+                            <button onClick={() => handleSuggested(invite.id, "accept",invite.name)} className="btn rounded-5 text-white btn-green">
+                                تایید
+                            </button>
+                            <button onClick={() => handleSuggested(invite.id, "reject",invite.name)} className="btn rounded-5 text-white btn-red">
+                                لغو
+                            </button>
+                        </div>
+                    </div>
+                ))
+            ) : (
+                <div className="d-flex bg-gray justify-content-between rounded-4 p-3 mt-3 mb-3">
+                    <span className="text-gray fs-5 fw-bold">
+                      هیچ دعوتی برای شما ثبت نشده است
+                    </span>
                 </div>
+            )}
+            <span className="fs-4 fw-bold d-block mt-3">شرکت های شما</span>
+          {companies.length > 0 ? (companies.map((company , index) =>{
+              setCompanyData(company)
+              return(
+                  <div key={index} className="d-flex bg-orange justify-content-between rounded-4 p-3 mt-3">
+                      <div className={"d-flex align-items-center gap-2"}>
+                      <span className={"text-white fs-5 fw-bold"}>
+                          {company.role === "owner" ? "مدیر:" : "کارمند:"}
+                      </span>
+                          <span className="text-white fs-5 fw-bold">{company?.name}</span>
+                      </div>
+                      <Link to={`${company.role === "owner" ? "/admin" : "/employee"}`} className="btn px-3 text-decoration-none text-white btn-green rounded-5">
+                          ورود
+                      </Link>
+                  </div>
+              )
+          })
+          ) : (
+              <div className="d-flex bg-gray justify-content-between rounded-4 p-3 mt-3 mb-3">
+                    <span className="text-gray fs-5 fw-bold">
+                      هیچ شرکتی برای شما ثبت نشده
+                    </span>
               </div>
-            ))}
-
-          <div className="d-flex bg-gray justify-content-between rounded-4 p-3 mt-3 mb-3">
-            <span className="text-gray fs-5 fw-bold">
-              هیچ دعوتی برای شما ثبت نشده است
-            </span>
-          </div>
-          {company.length > 0 && (
-            <>
-              <span className="fs-4 fw-bold ms-3 mt-3">شرکت های شما</span>
-              <div className="d-flex bg-orange justify-content-between rounded-4 p-3 mt-2">
-                <span className="text-white fs-5 fw-bold">شرکت رایاسان</span>
-
-                {role === "admin" && (
-                  <Link
-                    to="/admin-dashboard"
-                    className="btn fs-6 px-3 text-decoration-none text-white btn-green rounded-5"
-                  >
-                    ورود
-                  </Link>
-                )}
-
-                {role === "employee" && (
-                  <Link
-                    to="/registration-of-attendance"
-                    className="btn fs-6 px-3 text-decoration-none text-white btn-green rounded-5"
-                  >
-                    ورود
-                  </Link>
-                )}
-              </div>
-            </>
           )}
-
-          <div className="my-4 d-flex flex-column">
-            <span className="fs-4 fw-bold mt-3">
+          <div className="my-4 d-flex gap-4 flex-column">
+            <span className="fs-4 fw-bold ">
               کسب و کار خود را ایجاد کنید
             </span>
-            <div className="d-flex bg-orange rounded-4 p-3 mt-3">
-              <Link
-                to="/create-company"
-                className="btn fs-5 text-decoration-none text-white"
-              >
-                جهت ایجاد کسب و کار کلیک کنید
-              </Link>
-            </div>
+          <Link to="/create-companies" className="btn fs-5 text-decoration-none text-white bg-orange rounded-4 p-3 fw-bold">
+            جهت ایجاد کسب و کار کلیک کنید
+          </Link>
           </div>
-
-          <div className="d-flex flex-column">
-            <span className="fs-4 fw-bold my-3">
+          <div className="d-flex flex-column gap-4">
+            <span className="fs-4 fw-bold">
               درباره برنامه حضور و غیاب رایاسان
             </span>
             <p className="fs-5 text-justifyed">
@@ -157,7 +131,7 @@ export default function InviteBox() {
               دیگر نیازی به سیستم‌های حضور غیاب فیزیکی ندارید و به صورت آنلاین و
               بسیار دقیق‌تر می‌توانید ورود و خروج افراد را مدیریت کنید.
             </p>
-            <span className="fs-4 fw-bold my-3">حضور و غیاب رایاسان چیست؟</span>
+            <span className="fs-4 fw-bold ">حضور و غیاب رایاسان چیست؟</span>
             <p className="fs-5 text-justifyed">
               این برنامه به شما کمک می‌کند تا ورود و خروج کارمندان خود را در
               شرکت، کارگاه یا کارخانه مدیریت نمایید. با استفاده از این برنامه
@@ -166,7 +140,6 @@ export default function InviteBox() {
             </p>
           </div>
         </div>
-      </div>
     </Container>
   );
 }
